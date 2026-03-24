3f3d33b9e95650adc63debc60b1bf48b9c31984a
London-Grant <londongrant@outlook.com>
2026-03-23 23:38:07 -0400

    Starting Algorithim

--- src/app.py
+++ src/app.py
@@ -1,10 +1,44 @@
 import fastapi
 from pydantic import BaseModel
 from typing import Literal
+from enum import Enum
 
 app = fastapi.FastAPI()
 
+class Severity(Enum):
+    CRITICAL = 3
+    MODERATE = 2
+    MINOR = 1
 
+
+
+
+# symptom_id: HIGH/MED/LOW
+symptoms = {"s01": {"severity": Severity.CRITICAL},
+    "s02": {"severity": Severity.CRITICAL},
+    "s03": {"severity": Severity.CRITICAL},
+    "s04": {"severity": Severity.CRITICAL},
+    "s05": {"severity": Severity.CRITICAL},
+    "s06": {"severity": Severity.CRITICAL},
+    "s07": {"severity": Severity.CRITICAL},
+    "s08": {"severity": Severity.CRITICAL},
+    "s09": {"severity": Severity.CRITICAL},
+    "s10": {"severity": Severity.MODERATE},
+    "s11": {"severity": Severity.MODERATE},
+    "s12": {"severity": Severity.MODERATE},
+    "s13": {"severity": Severity.MODERATE},
+    "s14": {"severity": Severity.MODERATE},
+    "s15": {"severity": Severity.MODERATE},
+    "s16": {"severity": Severity.MODERATE},
+    "s17": {"severity": Severity.MINOR},
+    "s18": {"severity": Severity.MINOR},
+    "s19": {"severity": Severity.MINOR},
+    "s20": {"severity": Severity.MINOR},
+    "s21": {"severity": Severity.MINOR},
+    "s22": {"severity": Severity.MINOR},
+    "s23": {"severity": Severity.MINOR},
+}
+onset_weights = {'Sudden': .5, 'Rapid': .25, 'Gradual': -.5, 'Fluctuating': 0}
 
 
 class UrgencyModel(BaseModel):
@@ -12,12 +46,35 @@ class UrgencyModel(BaseModel):
     sex:str = Literal['Male', 'Female', 'Other']
     symptom_duration_num:int
     symptom_duration_qualifier: Literal['min', 'hrs', 'days', 'wks', 'mos']
-    symptom_onset:str
+    # symptom_onset:str = Literal['Sudden', 'Rapid', 'Gradual', 'Fluctuating']
+    symptom_onset:str = Literal(onset_weights.keys())
 
-    symptoms: list[str]
-    notes:str
+    symptoms: list[str] #The str is the id
+    notes:str # Realistically I probably do nothing with this. Maybe keyword search?
 
 
 @app.get("/urgency_score")
 def getUrgency(input:UrgencyModel):
-    return {"lol": "lmao"}
+    patient_symptoms = input.symptoms
+
+    freq_counter = {Severity.CRITICAL: 0, Severity.MODERATE: 0, Severity.MINOR: 0} 
+    for symptom in patient_symptoms: #TODO: Make these two lines more pythonic
+        freq_counter[symptoms[symptom]] +=1
+
+    # 3 seems kinda severe to me man, idk
+    if (freq_counter[Severity.CRITICAL] >= 3): return {"urgency": 10}
+    if (freq_counter[Severity.CRITICAL] == 2): return {"urgency": 9}
+
+    symptom_score = freq_counter[Severity.CRITICAL] * 4 + freq_counter[Severity.MODERATE]* 1 + freq_counter[Severity.MINOR] * .25 
+
+    # Put 50 as the median age. Any less slightly subtracts, any more slightly adds
+    # TODO: Make as a curve that biases old people, and children
+    symptom_score += (input.age - 50)/100.0
+
+    symptom_score += onset_weights[input.symptom_onset]
+
+    return {"urgency": min(10, round(symptom_score))}
+
+
+
+    
