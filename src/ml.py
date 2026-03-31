b48ade1eb5839f8b04582e4e85fb203cd3757228
London-Grant <londongrant@outlook.com>
2026-03-31 11:24:27 -0400

    Tweaking Pipeline

--- src/ml.py
+++ src/ml.py
@@ -11,19 +11,22 @@ X = regression_data[["onset", "severity"]].values
 y = regression_data[["mod"]].values
 
 print(regression_data.head())
-model = make_pipeline(
-    PolynomialFeatures(2), 
-    LinearRegression()
-    )
 
-model.fit(X, y)
+pipeline = Pipeline([
+    ("poly", PolynomialFeatures(2)), 
+    ("model", LinearRegression()),
+    # This next one makes sure the output is within certain bounds
+    ("clamp", FunctionTransformer(lambda X: numpy.clip(X, 0, 100)))
+])
+pipeline.fit(X, y)
 
 
 
-def getModifier(severity: int, onset: str = Literal[*discrete_onset]):
+
+def getModifier(severity: int, onset: str = Literal[*discrete_onset]):   
     global discrete_onset
     wrapped_onset = 1 +  discrete_onset.index(onset)
-    prediction = numpy.clip(model.predict([[wrapped_onset, severity]]), 0, 100)[0][0]
+    prediction = pipeline.predict([[wrapped_onset, severity]])[0][0]
     # print(prediction)
     return (prediction -  50)/25
 
