9a06bea7831532ce7e48c3b830ef7a58de2f4f63
AbraamIbrahim <abraamibr8@gmail.com>
2026-09-16 22:21:18 -0400

    added prod env and env var

--- NeuroFIRST/frontend/src/api.ts
+++ NeuroFIRST/frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL =  import.meta.env.VITE_API_URL ||"http://localhost:32425";
+const API_URL = import.meta.env.VITE_API_URL;
 console.log("API URL:", API_URL);
 
 const apiClient = axios.create({
